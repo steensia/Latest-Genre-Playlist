6b74d95271a5e2f74d78b5641afcd6ecb9a3c19c
Steen <steensia101@gmail.com>
2021-10-07 16:28:08 -0600

    Clarified workflow. Should implement for all genres first to test.

--- latest-genre-playlist.py
+++ latest-genre-playlist.py
@@ -3,13 +3,16 @@
 Application Workflow
 Step 1: Create list of genres
 Step 2: Create playlists for each genre in the list
-Step 3: Search for new released song(s) based on list of genres
-Step 4: Add new released song(s) to respective playlist
+Step 3: Search for new released song(s) based on list of genres (incorporate albums later)
+Step 4: Check if the song(s) were released today otherwise skip
+Step 4: Check genre of song(s) based on artists
+Step 4: Add new released song(s) to respective genre playlist
 Step 5: Repeat steps above in a crontab via AWS Lambda
 """
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import json
+from pprint import pprint
 
 class LatestGenrePlaylist:
     """ Initialize instance variables and SpotifyOAuth class to authenticate requests """  
@@ -29,6 +32,47 @@ class LatestGenrePlaylist:
                                          name = genre + " playlist",
                                          public = True)
             print(playlistId)
+    
+    """ Search for new released songs based on genre and return list of new songs """
+    def SearchNewReleases(self):
+        response = self.sp.new_releases(country = "US", limit = 1, offset = 3)
+
+        pprint(response)
+        # while response:
+        #     albums = response['albums']
+        #     for i, item in enumerate(albums['items'], 1):
+        #         if(item['album_type'] == "single"):
+        #             artists = item['artists']
+        #             names = []
+        #             for artist in artists:
+        #                 names.append(artist['name'])
+        #                 print(artist['id'])
+        #             #print(artists)
+        #             name = ", ".join(names)
+        #             print("#{}, Artist(s): {} Album Name: {} Album Type: {} Id: {}".format(
+        #                     albums['offset'] + i, 
+        #                     name,
+        #                     item['name'],
+        #                     item['album_type'], 
+        #                     item['id']))
+
+        #     response = None
+            #if albums['next']:
+            #    response = self.sp.next(albums)
+            #else:
+            #    response = None
+
+    def show_artist(self):
+        uri = "spotify:artist:3Y7RZ31TRPVadSFVy1o8os"
+        artist = self.sp.artist(uri)
+        for genre in artist['genres']:
+            if genre in self.genres:
+                print(genre)
+        pprint(artist)
+    
+    def get_album(self):
+        album = self.sp.album('spotify:album:055uuuPMs7soYjnONo02QV')
+        pprint(album)
 
     """ Helper function to retrieve list of genres from json file"""
     def __GetGenreList(self):
@@ -38,8 +82,18 @@ class LatestGenrePlaylist:
 
 if __name__ == '__main__':
     lgp = LatestGenrePlaylist()
-    lgp.CreateGenrePlayLists()
+    #lgp.CreateGenrePlayLists()
     #lgp.AddNewReleases()
+    #lgp.SearchNewReleases()
+    #lgp.show_artist()
+    lgp.get_album()
+
+
+
+
+
+
+
 
 
     
