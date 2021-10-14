111380526b2f639d7a2fce023b994216a6370b78
Steen <steensia101@gmail.com>
2021-10-13 20:37:00 -0600

    Deployed to AWS using Serverless Framework. Able to run code on Lambda
    Still need to fix logic, clean code and eventually update authentication method

--- latest-genre-playlist.py
+++ latest-genre-playlist.py
@@ -10,7 +10,7 @@ Step 4: Add new released song(s) to respective genre playlist
 Step 5: Repeat steps above in a crontab via AWS Lambda
 """
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy import SpotifyOAuth
 import boto3
 from botocore.exceptions import ClientError
 from boto3 import resource
@@ -145,31 +145,56 @@ def DatabaseDemo():
         for playlist in playlists['Items']:
             print(playlist['genre'], ":", playlist['id'])
 
-def EventHandler(event, context):
-    CLIENT_USERNAME = os.environ['SPOTIPY_CLIENT_USERNAME']
-    PLAYLIST_ID = os.environ['RECENT_LIKES_PLAYLIST_ID']
-
-    print("Authenticating to Spotify")
-    scope = "user-library-read playlist-modify-public"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-    print("Authenticated to Spotify")
-
-    print("Getting playlist tracks.")
-    results = sp.user_playlist_tracks(user=CLIENT_USERNAME, playlist_id=PLAYLIST_ID)
-    current_tracks = []
-    for track in results['items']:
-        current_tracks.append(track['track']['uri'])
-
-    print("Clearing playlist.")
-    sp.user_playlist_remove_all_occurrences_of_tracks(user=CLIENT_USERNAME, playlist_id=PLAYLIST_ID, tracks=current_tracks)
+def DatabaseDemo2(event):
+    dynamodb = boto3.resource("dynamodb", region_name='us-west-2')
+    table = dynamodb.Table("Playlists")
+    playlists = table.scan()['Items']
+    
+    genres = []
+    for playlist in playlists:
+        genre = playlist['genre']
+        genres.append(genre)
+        print(genre)
+    
+def handler(event, context):
+    print("Test handler")
+    DatabaseDemo2(event)
+    #CLIENT_USERNAME = os.environ['SPOTIPY_CLIENT_USERNAME']
+    #PLAYLIST_ID = os.environ['RECENT_LIKES_PLAYLIST_ID']
+    #PLAYLIST_LEN = int(os.environ['RECENT_LIKES_PLAYLIST_LEN'])
+    #
+    #print("Authenticating to Spotify")
+    #scope = "user-library-read playlist-modify-public"
+    #sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    #print("Authenticated to Spotify")
+    #
+    #print("Getting playlist tracks.")
+    #results = sp.user_playlist_tracks(user=CLIENT_USERNAME, playlist_id=PLAYLIST_ID)
+    #current_tracks = []
+    #for track in results['items']:
+    #    current_tracks.append(track['track']['uri'])
+    #
+    #print("Clearing playlist.")
+    #sp.user_playlist_remove_all_occurrences_of_tracks(user=CLIENT_USERNAME, playlist_id=PLAYLIST_ID, tracks=current_tracks)
+    #
+    #print("Get liked tracks.")
+    #results = sp.current_user_saved_tracks(limit=PLAYLIST_LEN)
+    #recent_likes = []
+    #for idx, item in enumerate(results['items']):
+    #    track = item['track']
+    #    print(idx, track['artists'][0]['name'], " – ", track['name'])
+    #    recent_likes.append(track['uri'])
+    #
+    #print("Add liked tracks.")
+    #sp.user_playlist_add_tracks(user=CLIENT_USERNAME, playlist_id=PLAYLIST_ID, tracks=recent_likes)
 
 if __name__ == '__main__':
     #lgp = LatestGenrePlaylist() 
     #lgp.AddNewReleases()
 
     #DatabaseDemo()
-
-    EventHandler("","")
+    
+    handler("","")
 
 
 
